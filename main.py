3debc25aef522aeefefffc0486767d50ddef07e9
freshremix <90133873+freshremix@users.noreply.github.com>
2022-09-30 16:02:45 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-os.system('python run.py')#The file to start the crawler project
-import ffmpeg
+import streamlit as st
+import pandas as pd
+import numpy as np
 from telegram.ext import Updater, MessageHandler, Filters, Handler
 from telegram import Bot
 import json
