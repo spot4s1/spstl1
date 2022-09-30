eeec90171c3104852e2b77ce57e8bd4f381eb45d
freshremix <90133873+freshremix@users.noreply.github.com>
2022-09-30 04:13:09 +0330

    Update main.py

--- main.py
+++ main.py
@@ -48,9 +48,9 @@ def get_single_song(bot, update):
     bot.send_message(chat_id=chat_id, text="Fetching...")
 
     if config["SPOTDL_DOWNLOADER"]:
-        os.system(f'spotdl {url}')
+        os.system(f'spotdl {url} --st 10 --dt 32 --output-format mp3')
     elif config["SPOTIFYDL_DOWNLOADER"]:
-        os.system(f'spotifydl {url} --st 10 --dt 32 --output-format mp3')
+        os.system(f'spotifydl {url}')
     else:
         logging.log(logging.ERROR, 'you should select one of downloaders')
 
