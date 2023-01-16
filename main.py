ffba754aa13756a083daaf9aea000b1064f12008
spot4s1 <110562663+spot4s1@users.noreply.github.com>
2023-01-16 13:20:19 +0330

    Update main.py

--- main.py
+++ main.py
@@ -44,7 +44,7 @@ def get_single_song(update, context):
     context.bot.send_message(chat_id=chat_id, text="Fetching...")
 
     if config["SPOTDL_DOWNLOADER"]:
-        os.system(f'spotdl download {url} --threads 12 --format mp3 --bitrate 320k --ffmpeg /home/mrkk68w/.spotdl/ffmpeg')
+        os.system(f'spotdl download {url} --threads 2 --format mp3 --bitrate 320k --ffmpeg /home/mrkk68w/.spotdl/ffmpeg')
     elif config["SPOTIFYDL_DOWNLOADER"]:
         os.system(f'spotifydl {url}')
     else:
